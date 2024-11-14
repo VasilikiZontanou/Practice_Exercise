514736066b8c0e56c60b0c6c17850b3d2507a75b
FLORIAN LAMAJ <t8230071@aueb.gr>
2024-11-14 13:03:57 +0200

    oristhiki am

--- Exercise.java
+++ Exercise.java
@@ -1,7 +1,8 @@
 public class Exercise {
 
     public static void main(String[] args) {
-        System.out.println("8230039");
+        System.out.println(8230039);
         System.out.println(8230129);
+        System.out.println(8230071);
     }
 }
